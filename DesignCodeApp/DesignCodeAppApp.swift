9623448d59cb2378bcf188306c8d561f7147c500
Saeed Noshadi <SaeedNoshadi89@gmail.com>
2022-07-27 16:26:13 +0430

    first commit

---
+++ DesignCodeApp/DesignCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeAppApp.swift
+//  DesignCodeApp
+//
+//  Created by $@€€© on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
