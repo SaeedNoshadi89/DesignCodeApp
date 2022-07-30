d97763f855022293c7a4400687e04a5d96431c3d
Saeed Noshadi <SaeedNoshadi89@gmail.com>
2022-07-30 18:56:02 +0430

    create the first of card

--- DesignCodeApp/ContentView.swift
+++ DesignCodeApp/ContentView.swift
@@ -9,13 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20.0, style: .continuous))
+            
+            Text("SwiftUi for IOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            
+            Text("Build an IOS app for IOS 15 with custom layout, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            
+            
+        }
+        .padding(.all, 20.0)
+        .padding(.vertical, 20.0)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30.0, style: .continuous))
+//        .mask(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(RoundedRectangle(cornerRadius: 30.0, style: .continuous).stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+            .blendMode(.overlay)
+        )
+//        .overlay(RoundedRectangle(cornerRadius: 30.0, style: .continuous).stroke(.black, style: .init(lineWidth: 5.0, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [50.0, 10.0], dashPhase: 30.0)))
+        .padding(.horizontal, 20.0)
+        .background(Image("Blob 1")
+            .offset(x: 250, y: -100))
+        .overlay(Image("Illustration 5")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 230)
+            .offset(x: 32, y: -90)
+        )
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
