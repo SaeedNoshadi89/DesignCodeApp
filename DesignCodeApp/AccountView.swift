4d1e140cba762b9ed5e55c03cccc3c13e2aead9d
Saeed Noshadi <SaeedNoshadi89@gmail.com>
2022-08-03 21:18:14 +0430

    add navigation link and swipe actions

--- DesignCodeApp/AccountView.swift
+++ DesignCodeApp/AccountView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    
     var body: some View {
         NavigationView {
             List{
@@ -21,15 +23,15 @@ struct AccountView: View {
                         .background(Circle().fill(.ultraThinMaterial))
                         .background(
                             Image(systemName: "hexagon")
-                            .symbolVariant(.fill)
-                            .foregroundColor(.blue)
-                            .font(.system(size: 200))
-                            .offset(x: -50, y: -100)
-                    )
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                        )
                     Text("Saeed Noshadi")
                         .font(.system(.title, design: .rounded).weight(.semibold))
-//                    Label("Iran", systemImage: "location")
-//                        .foregroundColor(.secondary)
+                    //                    Label("Iran", systemImage: "location")
+                    //                        .foregroundColor(.secondary)
                     HStack{
                         Image(systemName: "location")
                             .imageScale(.large)
@@ -40,13 +42,56 @@ struct AccountView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
                 
+                
+                //MARK: Section One
                 Section {
-                    Label("Settings", systemImage: "gear")
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
+                    NavigationLink {ContentView()} label: {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink { Text("Billing") } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink { ContentView() } label: {
+                        Label("Help", systemImage: "questionmark")
+                    }
                 }
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
+                
+                //MARK: Section Two
+                Section{
+                    Link(destination: URL(string: "https://apple.com")!){
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .accentColor(.blue)
+                        }
+                    }
+                    if !isDeleted {
+                        Link(destination: URL(string: "https://youtube.com")!){
+                            HStack{
+                                Label("Youtube", systemImage: "tv")
+                                Spacer()
+                                Image(systemName: "link")
+                                    .accentColor(.blue)
+                            }
+                        }
+                        .swipeActions(edge: .leading, allowsFullSwipe: true){
+                            Button(action: { isDeleted = true }) {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .tint(.red)
+                            
+                            Button {} label: {
+                                Label("Pin", systemImage: "pin")
+                            }
+                        }
+                    }
+                }
+                .accentColor(.primary)
+                .listRowSeparator(.hidden)
+                
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
